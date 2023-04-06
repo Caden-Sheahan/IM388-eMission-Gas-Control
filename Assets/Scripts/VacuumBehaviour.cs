115cf47578e9888f16200b1b23f19c8cbc0babbd
Charlie Polonus <capolonus@gmail.com>
2023-04-06 16:03:49 -0500

    better vacuum

--- Assets/Scripts/VacuumBehaviour.cs
+++ Assets/Scripts/VacuumBehaviour.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class VacuumBehaviour : MonoBehaviour
 {
+    public enum VacuumShape { Capsule, Sphere }
+
+    [SerializeField] private VacuumShape _shape;
     [SerializeField] private LayerMask _gasMask;
     [SerializeField] private bool _activated;
 
+    [SerializeField] private float _rangeSize;
+    [SerializeField] private float _rangeLength;
+    [SerializeField] private float _disableRadius;
     [SerializeField] private float _strength;
 
     // Update is called once per frame
@@ -14,15 +20,26 @@ public class VacuumBehaviour : MonoBehaviour
     {
         if (_activated)
         {
-            RaycastHit[] inCapsule = Physics.CapsuleCastAll(transform.position, transform.position + transform.forward, 0.5f, transform.forward, 5, _gasMask);
-            RaycastHit[] inSphere = Physics.SphereCastAll(transform.position, 10, Vector3.up, _gasMask);
+            RaycastHit[] inRange;
+
+            switch (_shape)
+            {
+                case VacuumShape.Capsule:
+                    inRange = Physics.CapsuleCastAll(transform.position, transform.position + transform.forward, _rangeSize / 2f, transform.forward, _rangeLength, _gasMask);
+                    break;
+                default:
+                    inRange = Physics.SphereCastAll(transform.position, _rangeLength, Vector3.up, _gasMask);
+                    break;
+            }
+
+            RaycastHit[] inSphere = Physics.SphereCastAll(transform.position, _disableRadius, Vector3.up, _gasMask);
 
             foreach (RaycastHit curGas in inSphere)
             {
                 curGas.transform.GetComponent<GasBehaviour>().MovingToVacuum(false);
             }
 
-            foreach (RaycastHit curGas in inCapsule)
+            foreach (RaycastHit curGas in inRange)
             {
                 curGas.transform.GetComponent<GasBehaviour>().MovingToVacuum(true, _strength, transform);
             }
