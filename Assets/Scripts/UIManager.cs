c01d732913804be7e2585f469cb448316689d7f4
Caden-Sheahan <sheahancj@outlook.com>
2023-04-24 15:29:31 -0500

    Audio Changes and additional UI

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
@@ -10,6 +11,8 @@ public class UIManager : MonoBehaviour
     public Slider gasCount; // slider showing how much gas there is in game
     public RectTransform winZone; // slider zone for winning
     public RectTransform loseZone; // slider zone for losing
+    public GameObject endTimer;
+    public TMP_Text endTimerText;
 
     public PlayerMovement pm;
 
@@ -19,6 +22,39 @@ public class UIManager : MonoBehaviour
         gasCount.value = GasManager.main.GasRatio;
         winZone.sizeDelta = new Vector2(600 * GameManager.main.WinRatio, 10);
         loseZone.sizeDelta = new Vector2(600 * GameManager.main.LoseRatio, 10);
+
+        switch (GameManager.main.Status)
+        {
+            case 0: // middle
+                {
+                    endTimer.SetActive(false);
+                    break;
+                }
+            case 1: // winning
+                {
+                    endTimer.SetActive(true);
+                    endTimerText.text = "You will win in " + (Mathf.FloorToInt(GameManager.main.CurTime) + 1) + " seconds";
+                    endTimerText.color = new Color(0.639f, 0.866f, 0.608f);
+                    break;
+                }
+            case -1: // losing
+                {
+                    endTimer.SetActive(true);
+                    endTimerText.text = "You will lose in " + (Mathf.FloorToInt(GameManager.main.CurTime) + 1) + " seconds";
+                    endTimerText.color = new Color(0.866f, 0.624f, 0.608f);
+                    break;
+                }
+            default:
+                {
+                    print("End text brokey");
+                    break;
+                }
+        }
+
+        if (GameManager.main.CurTime <= 0)
+        {
+            endTimer.SetActive(false);
+        }
     }
 
     #region Button Functions
